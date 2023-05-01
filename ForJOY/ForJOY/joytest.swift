b3e2a2ae0aad2b5219ecc2c17d0908aa6f4e9158
Joy19061618 <hosic119@nate.com>
2023-05-01 17:10:42 +0900

    joy test

---
+++ ForJOY/ForJOY/joytest.swift
@@ -0,0 +1,20 @@
+//
+//  joytest.swift
+//  ForJOY
+//
+//  Created by 조호식 on 2023/05/01.
+//
+
+import SwiftUI
+
+struct joytest: View {
+    var body: some View {
+        Text("joy loves joy")
+    }
+}
+
+struct joytest_Previews: PreviewProvider {
+    static var previews: some View {
+        joytest()
+    }
+}
