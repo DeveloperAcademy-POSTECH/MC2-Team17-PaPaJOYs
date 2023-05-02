020a7eec96cc2f9ed392b0b285bbaed271dc1be8
NewYorkKim <newyorkkim1030@gmail.com>
2023-05-02 11:27:52 +0900

    [NewYork] Test: 브랜치 테스트

---
+++ ForJOY/ForJOY/NYTest.swift
@@ -0,0 +1,20 @@
+//
+//  NYTest.swift
+//  ForJOY
+//
+//  Created by Nayeon Kim on 2023/05/02.
+//
+
+import SwiftUI
+
+struct NYTest: View {
+    var body: some View {
+        Text("Hello, I'm NewYork!")
+    }
+}
+
+struct NYTest_Previews: PreviewProvider {
+    static var previews: some View {
+        NYTest()
+    }
+}
