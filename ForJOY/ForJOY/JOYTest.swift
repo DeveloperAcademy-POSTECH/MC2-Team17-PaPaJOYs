a30c21a3eb337d6c9e0930eccaf961ceb8bcf989
hosic <128379323+Joy19061618@users.noreply.github.com>
2023-05-03 08:58:38 +0900

    테스트 파일 삭제

--- ForJOY/ForJOY/JOYTest.swift
+++ ForJOY/ForJOY/JOYTest.swift
@@ -5,16 +5,3 @@
 //  Created by 조호식 on 2023/05/02.
 //
 
-import SwiftUI
-
-struct JOYTest: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct JOYTest_Previews: PreviewProvider {
-    static var previews: some View {
-        JOYTest()
-    }
-}
