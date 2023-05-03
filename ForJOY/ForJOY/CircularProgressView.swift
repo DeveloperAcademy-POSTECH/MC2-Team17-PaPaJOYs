ecb0eff77e5b574e8124437889b1dd7fd62bdd04
olive <olionoleo@gmail.com>
2023-05-03 16:30:38 +0900

    all annotation olive code

--- ForJOY/ForJOY/CircularProgressView.swift
+++ ForJOY/ForJOY/CircularProgressView.swift
@@ -1,43 +1,43 @@
+////
+////  CircularProgressView.swift
+////  ForJOY
+////
+////  Created by Sehui Oh on 2023/05/03.
+////
 //
-//  CircularProgressView.swift
-//  ForJOY
+//import SwiftUI
 //
-//  Created by Sehui Oh on 2023/05/03.
 //
-
-import SwiftUI
-
-
-struct CircularProgressView: View {
-    
-    @Binding var recProgress: Double
-    
-    var body: some View {
-        
-        
-        ZStack{
-            
-            Circle() // Progress 1
-                .stroke(
-                    Color("JoyBlue").opacity(0.5),
-                    lineWidth: 2)
-                .frame(width: 260)
-            Circle() // Progress 2
-                .trim(from: 0, to: recProgress)
-                .stroke(
-                    Color("JoyBlue"),
-                    style: StrokeStyle(
-                        lineWidth: 5,
-                        lineCap: .round))
-                    .frame(width: 260)
-                    .rotationEffect(.degrees(-90))//What Degree is Proper? Clockwise?
-                    .animation(.easeInOut(duration: 2), value: recProgress)
-        }
-    }//Body End
-}//Struct End
-
-//struct CircularProgressView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CircularProgressView()
-//    }
-//}
+//struct CircularProgressView: View {
+//
+//    @Binding var recProgress: Double
+//
+//    var body: some View {
+//
+//
+//        ZStack{
+//
+//            Circle() // Progress 1
+//                .stroke(
+//                    Color("JoyBlue").opacity(0.5),
+//                    lineWidth: 2)
+//                .frame(width: 260)
+//            Circle() // Progress 2
+//                .trim(from: 0, to: recProgress)
+//                .stroke(
+//                    Color("JoyBlue"),
+//                    style: StrokeStyle(
+//                        lineWidth: 5,
+//                        lineCap: .round))
+//                    .frame(width: 260)
+//                    .rotationEffect(.degrees(-90))//What Degree is Proper? Clockwise?
+//                    .animation(.easeInOut(duration: 2), value: recProgress)
+//        }
+//    }//Body End
+//}//Struct End
+//
+////struct CircularProgressView_Previews: PreviewProvider {
+////    static var previews: some View {
+////        CircularProgressView()
+////    }
+////}
