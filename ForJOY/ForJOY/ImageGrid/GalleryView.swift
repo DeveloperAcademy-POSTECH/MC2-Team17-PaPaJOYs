6cecabbe63ef361990ff43bc619d91388b852088
ChaeeunShin <shince0805@gmail.com>
2023-05-09 16:46:49 +0900

    [Ash] Feat : 연도별 앨범뷰, 갤러리뷰 연결

--- ForJOY/ForJOY/ImageGrid/GalleryView.swift
+++ ForJOY/ForJOY/ImageGrid/GalleryView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct GalleryView: View {
+//    init() {
+//        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+//        UINavigationBar.appearance().shadowImage = UIImage()
+//        }
     var columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 3), count: 3)
     let testImg = ["test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8"]
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 3) {
-                ForEach(testImg, id: \.self) { name in
-                    Image(name)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: screenWidth / 3 - 3, height: screenWidth / 3 - 3)
-                        .clipped()
+        NavigationStack {
+            HStack {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 3) {
+                        ForEach(testImg, id: \.self) { name in
+                            Image(name)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: screenWidth / 3 - 3, height: screenWidth / 3 - 3)
+                                .clipped()
+                        }
+                    }
+                }
+            }
+            .navigationTitle("#조이서")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "mic.circle.fill")
+                        .font(.title2)
+                        .foregroundColor(Color(hex: "659BD5"))
                 }
             }
         }
