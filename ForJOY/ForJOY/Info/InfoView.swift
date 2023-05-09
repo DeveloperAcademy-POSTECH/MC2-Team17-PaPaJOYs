727bb01fc5c82e9903d7192c6ee05143ee11fed1
Hyebin (Helia) <hyebin218@naver.com>
2023-05-09 14:18:21 +0900

    [NewYork&Helia] Feat: InfoView 구현

---
+++ ForJOY/ForJOY/Info/InfoView.swift
@@ -0,0 +1,64 @@
+//
+//  InfoView.swift
+//  ForJOY
+//
+//  Created by hyebin on 2023/05/09.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    @State var title: String = ""
+    @State var date = Date()
+    @State var tag: String?
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+                //TODO: 이미지 받아서 보여주기
+                Rectangle()
+                    .fill(.black)
+                    .padding(32)
+                
+                List {
+                    HStack{
+                        Text("제목")
+                        Spacer(minLength: 220)
+                        TextField("제목", text: $title)
+                            .frame(alignment: .trailing)
+                    }
+                    .listRowBackground(Color("JoyWhite"))
+                    
+                    HStack{
+                        Text("태그")
+                        Spacer(minLength: 230)
+                        NavigationLink(destination: InfoTagView(tag: $tag), label: {
+                            if tag == nil {
+                                Text("없음")
+                            }else {
+                                Text(tag!)
+                            }
+                        })
+                    }
+                    .listRowBackground(Color("JoyWhite"))
+                    
+                    DatePicker(
+                        "날짜",
+                        selection: $date,
+                        displayedComponents: [.date]
+                    )
+                    .tint(Color("JoyBlue"))
+                    .listRowBackground(Color("JoyWhite"))
+                }
+                .scrollContentBackground(.hidden)
+            }
+            .background(Color("JoyDarkG"))
+        }
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
