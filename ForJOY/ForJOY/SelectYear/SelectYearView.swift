0c3ad40588f06ef865234b3d77a3181207bb7922
ChaeeunShin <96675556+chaen805@users.noreply.github.com>
2023-05-11 12:17:48 +0900

    Revert "[Ash] Feat : 태그 이용한 필터링 기능 구현"

--- ForJOY/ForJOY/SelectYear/SelectYearView.swift
+++ ForJOY/ForJOY/SelectYear/SelectYearView.swift
@@ -5,33 +5,16 @@
 //  Created by Chaeeun Shin on 2023/05/02.
 //
 
-// 다음 페이지와 연결될 수 있도록 데이터 고민필요..
-// 데이터를 불러올 때 필터링 하는 방향 고려
+// 뷰만 그려놓음, 기능 구현 필요!!
 
 import SwiftUI
 
 struct SelectYearView: View {
     @State var isNewest = true
-    @State var selection = "All"
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color("JoyDarkG")
-                    .ignoresSafeArea()
-                VStack(spacing: 20) {
-                    HeaderView(isNewest: $isNewest)
-                    HStack {
-                        Spacer()
-                        TagView(selection: $selection)
-                        Spacer()
-                    }
-                    HStack {
-                        Spacer()
-                        AlbumView(isNewest: $isNewest, selection: $selection)
-                        Spacer()
-                    }
-                }
-            }
+        VStack(spacing: 20) {
+            HeaderView(isNewest: $isNewest)
+            AlbumView(isNewest: $isNewest)
         }
     }
 }
@@ -42,14 +25,12 @@ struct HeaderView: View {
     var body: some View {
         VStack(spacing: 10) {
             HStack {
-                Spacer()
-                // 녹음하는 뷰와 이어지는 NavigationLink로 변경 예정
                 Button(action: {}) {
-                    Image(systemName: "mic.circle.fill")
-                        .font(.title2)
-                        .foregroundColor(Color(hex: "659BD5"))
+                    Image(systemName: "plus")
                 }
-                Menu {
+                .padding(.leading, 20)
+                Spacer()
+                Menu("정렬") {
                     Button(action: {isNewest = true}) {
                         HStack {
                             Text("최근부터 보기")
@@ -66,81 +47,43 @@ struct HeaderView: View {
                             Image(systemName: "checkmark")
                         }
                     }
-                } label: {
-                    Image(systemName: "slider.horizontal.3")
-                        .font(.title2)
-                        .foregroundColor(Color(hex: "659BD5"))
                 }
                 .padding(.trailing, 20)
             }
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(viewModel.testName, id: \.self) { name in
+                        TagView(name: name)
+                    }
+                }
+            }
+            .frame(width: screenWidth * 0.4)
         }
     }
 }
 
 struct TagView: View {
-    @ObservedObject var viewModel = TestViewModel()
-    @Binding var selection: String
-    @State var isAllSelect = true
-    
-    // 데이터를 태그 단위로 묶어주기 - DB 연결시 변경 가능
-    var TagGroup: [String: [TestModel]] {
-        var data = Dictionary(grouping: viewModel.testData) { i in
-            i.tagName
-        }
-        for(key, value) in data {
-            data[key] = value.sorted(by: {$0.idx > $1.idx})
-        }
-        return data
-    }
-    
-    var tagKey: [String] {
-        TagGroup.map({ $0.key }).sorted()
-    }
-
+    @State var isSelected = false
+    let name: String
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 5) {
-                Button {
-                    selection = "All"
-                    isAllSelect = true
-                } label: {
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 75, height: 30)
-                        .foregroundColor(isAllSelect ? Color("JoyYellow") : Color(hex: "F2F2F7"))
-                        .opacity(0.9)
-                        .overlay(
-                            Text("모든태그")
-                                .font(.subheadline)
-                                .foregroundColor(.black)
-                                .lineLimit(1)
-                        )
-                }
-                ForEach(tagKey, id: \.self) { i in
-                    Button {
-                        selection = i
-                        isAllSelect = false
-                    } label: {
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(width: 75, height: 30)
-                            .foregroundColor(isAllSelect ? Color(hex: "F2F2F7") : (selection == i ? Color("JoyYellow") : Color(hex: "F2F2F7")))
-                            .opacity(0.9)
-                            .overlay(
-                                Text("#" + i)
-                                    .font(.subheadline)
-                                    .foregroundColor(.black)
-                                    .lineLimit(1)
-                            )
-                    }
-                }
+        Text(name)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .padding(10)
+            .lineLimit(1)
+            .background(isSelected ? Color.yellow : Color.gray)
+            .frame(height: 25)
+            .cornerRadius(7)
+            .onTapGesture {
+                isSelected.toggle()
             }
-        }
     }
 }
 
 struct AlbumView: View {
     @ObservedObject var viewModel = TestViewModel()
     @Binding var isNewest: Bool
-    @Binding var selection: String
     
     // 데이터를 연도 단위로 묶어주기
     var YearGroup: [String: [TestModel]] {
@@ -150,65 +93,31 @@ struct AlbumView: View {
         for(key, value) in data {
             data[key] = value.sorted(by: {$0.idx > $1.idx})
         }
-        
-        if selection == "All" {
-            return data
-        } else {
-            let filteredData = data.filter({ $0.value.contains(where: { $0.tagName == selection })})
-            let filteredByTag = filteredData.mapValues { value in
-                value.filter{ $0.tagName == selection }
-            }
-            return filteredByTag
-        }
+        return data
     }
     var yearKey: [String] {
-        let temp = YearGroup.map({ $0.key }).sorted()
+        var temp = YearGroup.map({ $0.key }).sorted()
         
         return isNewest ? temp.reversed() : temp
     }
-    
-    var columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 12), count: 2)
+    // Grid를 사용하기 위한..?!
+    var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: columns, spacing: 15) {
+            LazyVGrid(columns: columns) {
                 ForEach(yearKey, id: \.self) { i in
-                    let filteredData = (selection == "All" ? YearGroup[i]! : YearGroup[i]!.filter{ $0.tagName == selection })
-                    NavigationLink(destination: GalleryView(tagName: selection, year: i, imageName: filteredData.map{ $0.imageName })) {
-                        AlbumSubView(imageName: YearGroup[i]![0].imageName, year: i)
+                    VStack {
+                        Button(action: {}) {
+                            YearGroup[i]![0].image
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        Text(i)
                     }
                 }
             }
         }
-    }
-}
-
-struct AlbumSubView: View {
-    let imageName: String
-    let year: String
-    var body: some View {
-        ZStack {
-            Color("JoyWhite")
-            VStack {
-                Image(imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: screenWidth * 0.4, height: screenWidth * 0.4)
-                    .clipped()
-                    .cornerRadius(10)
-                    .padding(10)
-                HStack {
-                    Spacer()
-                    Text(year)
-                        .font(.headline)
-                        .foregroundColor(Color("JoyDarkG"))
-                        .padding(.top, -10)
-                        .padding(.trailing, 15)
-                }
-                .padding(.bottom, 10)
-            }
-        }
-        .cornerRadius(10)
     }
 }
 
