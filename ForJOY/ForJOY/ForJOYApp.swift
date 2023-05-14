10681c46d513a2c128ec4930a841c8e665920b22
olive <olionoleo@gmail.com>
2023-05-14 20:26:43 +0900

    [Olive] Merge : 충돌로 인한 새 브랜치 생성 및 forCombine merge

--- ForJOY/ForJOY/ForJOYApp.swift
+++ ForJOY/ForJOY/ForJOYApp.swift
@@ -16,7 +16,7 @@ struct ForJOYApp: App {
             ContentView()
 //            CameraView()
 //                .environmentObject(GlobalStore) //GlobalStore.swift 의 ObservableObject/Published
-                .environmentObject(GlobalStore) //GlobalStore.swift 의 ObservableObject/Published
+//                .environmentObject(GlobalStore) //GlobalStore.swift 의 ObservableObject/Published
 //            SelectYearView()
 //                .accentColor(.white)
         }
