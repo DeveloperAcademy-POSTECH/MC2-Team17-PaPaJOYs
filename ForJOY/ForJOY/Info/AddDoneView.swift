71828294c56cde4d5ebc0448503138fba5c9e900
olive <olionoleo@gmail.com>
2023-05-15 06:22:29 +0900

    [Olive] Feat : AddDoneView 레이아웃 수정

--- ForJOY/ForJOY/Info/AddDoneView.swift
+++ ForJOY/ForJOY/Info/AddDoneView.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct AddDoneView: View {
     var body: some View {
-        VStack{
-            LottieView(jsonName: "SaveComplete")
-                .frame(width: 1000, height: 1000)
-            Text("추억이 저장되었어요")
-                .foregroundColor(Color("JoyWhite"))
-        }
-    }
-}
+        ZStack{
+            Color("JoyDarkG")
+                .ignoresSafeArea()
+            ZStack{
+                VStack{
+                    LottieView(jsonName: "SaveComplete")
+                        .frame(width: 500, height: 500)
+                    Spacer()
+                }
+                .frame(width: 400, height: 600)
+                
+                VStack{
+                    Spacer()
+                    Text("소중한 추억이 저장되었어요")
+                        .foregroundColor(Color("JoyWhite"))
+                }
+                .frame(width: 400, height: 300)
+            }
+
+
+        }//Zstack END
+    }//BODY END
+}//Struct END
 
 struct AddDoneView_Previews: PreviewProvider {
     static var previews: some View {
