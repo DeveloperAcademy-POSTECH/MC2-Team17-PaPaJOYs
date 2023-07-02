3977a4281bedfae0fc610b353054512abb5b02ba
Hyebin (Helia) <hyebin218@naver.com>
2023-07-02 20:55:35 +0900

    [Helia] Feat: view 순서 변경 및 카메라 수정

--- ForJOY/ForJOY/Voice/Views/VoiceView.swift
+++ ForJOY/ForJOY/Voice/Views/VoiceView.swift
@@ -12,17 +12,12 @@ struct VoiceView: View {
     @StateObject var voiceViewModel = VoiceViewModel()
     @StateObject var realmManger = RealmManger()
     
-    @State private var showingAlert = false
     @State private var effect1 = false
     @State private var effect2 = false
     @State var recording: URL?
-    @State var isButtonOn = false
-    @State var isShowActionSheet = false
-    @State var isShowPhotoPicker = false
-    @State private var selectedItem = [PhotosPickerItem]()
-    @State private var selectedImage: UIImage?
     @State var isChoosen = false
-    @State var isHiddentButton = false
+    
+    @Binding var selectedImage: UIImage?
     
     var body: some View {
         NavigationStack{
@@ -31,15 +26,14 @@ struct VoiceView: View {
                 VStack {
                     Spacer()
                     Button(action: {
-                        isShowActionSheet = true
-                        isHiddentButton = true
+                        isChoosen = true
                     }, label: {
-                        if !voiceViewModel.isRecording && voiceViewModel.isEndRecording && !isHiddentButton{
+                        if !voiceViewModel.isRecording && voiceViewModel.isEndRecording {
                             RoundedRectangle(cornerRadius: 15)
                                 .fill(Color("JoyWhite"))
                                 .frame(width: screenWidth * 0.9, height: 60)
                                 .overlay(
-                                    Text("사진 담기")
+                                    Text("완료")
                                         .font(.system(size: 16))
                                         .fontWeight(.bold)
                                         .foregroundColor(Color("JoyDarkG"))
@@ -49,62 +43,6 @@ struct VoiceView: View {
                     .frame(width: 400, height: 250)
                     .onChange(of: voiceViewModel.recording) { newValue in
                         recording = newValue
-                    }
-                    .confirmationDialog("test", isPresented: $isShowActionSheet) {
-                        NavigationLink(destination: CameraView(recording: $recording), label: {
-                            Button(action: {
-                                isHiddentButton = false
-                            }, label: {
-                                Text("사진 찍으러 가기")
-                                    .foregroundColor(Color("JoyBlue"))
-                            })
-                            .background(Color("JoyWhite"))
-                        })
-                        .onAppear(){
-                            isHiddentButton = false
-                        }
-                        
-                        Button(action: {
-                            isShowPhotoPicker.toggle()
-                            isHiddentButton = false
-                        }, label: {
-                            Text("사진 고르러 가기")
-                                .foregroundColor(Color("JoyBlue"))
-                        })
-                        .background(Color("JoyWhite"))
-                        
-                        
-                        Button(role: .cancel, action: {
-                            isHiddentButton = false
-                        }, label: {
-                            Text("Cancel")
-                        })
-                    }
-                    .photosPicker(
-                        isPresented: $isShowPhotoPicker,
-                        selection: $selectedItem,
-                        maxSelectionCount: 1,
-                        matching: .images
-                    )
-                    .preferredColorScheme(.dark)
-                    .tint(Color("JoyBlue"))
-                    .onChange(of: selectedItem) { newValue in
-                        guard let item = selectedItem.first else { return }
-                        item.loadTransferable(type: Data.self) { result in
-                            switch result {
-                            case .success(let data):
-                                if let data = data {
-                                    selectedImage = UIImage(data: data)
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                        isChoosen = true
-                                    }
-                                } else {
-                                    print("Data is nill!")
-                                }
-                            case .failure(let failure):
-                                fatalError("\(failure)")
-                            }
-                        }
                     }
                 }
 
