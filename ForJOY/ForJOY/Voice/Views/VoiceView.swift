f06d7df6c9985f3176475501462efc8bf681d07b
ChaeeunShin <shince0805@gmail.com>
2023-08-19 17:50:37 +0900

    [Ash] Chore: 녹음 관련 앱그룹 수정

--- ForJOY/ForJOY/Voice/Views/VoiceView.swift
+++ ForJOY/ForJOY/Voice/Views/VoiceView.swift
@@ -10,6 +10,7 @@ import PhotosUI
 
 // 사용자의 목소리 녹음 기능을 제공하는 뷰
 struct VoiceView: View {
+    @Environment(\.dismiss) private var dismiss
     // 목소리 녹음과 관련된 뷰 모델
     @StateObject var voiceViewModel = VoiceViewModel()
     
@@ -19,16 +20,11 @@ struct VoiceView: View {
     // 녹음 파일의 URL
     @State var recording: URL?
     // 다음 화면으로 이동하기 위한 상태 변수
-    @State var isChoosen = false
+    @State var endRecording = false
     
     // 선택된 이미지 (부모 뷰에서 전달 받을 값)
     @Binding var selectedImage: UIImage?
-    
-    @State private var isBackButtonPressed = false // 이 상태 변수를 추가합니다.
-    
-    
-    
-    
+
     
     var body: some View {
         NavigationStack{
@@ -65,7 +61,7 @@ struct VoiceView: View {
                             
                             if !voiceViewModel.isRecording && voiceViewModel.isEndRecording {
                                 Button(action: {
-                                    isChoosen = true
+                                    endRecording = true
                                     DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                                         
                                         // 여기에 InfoView로의 네비게이션 코드를 추가
@@ -92,45 +88,27 @@ struct VoiceView: View {
                     }
                     
                     NavigationLink(
-                        destination: InfoView(selectedImage: $selectedImage, recording: $recording)
-                            .navigationBarBackButtonHidden(),
-                        //                        .environmentObject(realmManger),
-                        isActive: $isChoosen
+                        destination: InfoView(selectedImage: $selectedImage, recording: $recording),
+
+                        isActive: $endRecording
                     ){}
                         .isDetailLink(false)
-                    
-                    // 뒤로가기 버튼을 위한 네비게이션 링크
-                    NavigationLink(
-                        destination: SelectYearView(),
-                        isActive: $isBackButtonPressed
-                    ) {
-                        EmptyView()
-                    }
                 }
                 .padding(.top, 95)
                 
-                
                 .navigationBarBackButtonHidden(true)
-                .navigationTitle("")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button(action: {
-                            isBackButtonPressed = true  // 버튼 클릭 시 상태 변수를 true로 설정
-                        }) {
-                            HStack {
-                                Image(systemName: "chevron.backward")
-                                    .foregroundColor(Color("JoyBlue"))
-                                    .padding(.top, 65)
-                                Text("사진 선택")
-                                    .fontWeight(.bold)
-                                    .foregroundColor(Color("JoyBlue"))
-                                    .padding(.top, 65)
-                                
-                            }
-                        }
-                    }
-                }
+                .navigationBarItems(leading: BackButton)
+
             }
         }
+    }
+    
+    private var BackButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Text("\(Image(systemName: "chevron.backward")) 사진 선택")
+        }
+
     }
 }
